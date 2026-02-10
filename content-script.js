6abf1b0cb301b667e0b2baafa6d5ba7bf51648f9
DeeKahy <97156953+DeeKahy@users.noreply.github.com>
2026-02-10 14:23:35 +0100

    Update content-script.js

--- content-script.js
+++ content-script.js
@@ -101,10 +101,7 @@
 
   function appendFuzzyToTarget(e) {
     if (e.target.classList.contains("multiselect__input")) {
-      // When text is selected and we write a letter it wrongly gets appended to the end of the text instead of replacing it
-      // To prevent this, we only append the tilde when nothing is selected
       if (e.target.selectionStart === e.target.selectionEnd) {
-        // Don't append the tilde if the first character is already a tilde or we start with a space (so we can opt out of fuzzy search)
         if (!e.target.value.startsWith("~") && !e.target.value.startsWith(" ") && e.key !== " ") {
           e.target.value = "~" + e.target.value;
         }
