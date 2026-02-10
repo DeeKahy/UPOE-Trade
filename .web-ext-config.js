7b0d5de696491e0b0adf42e1e01c55aba1fbeb85
DeeKahy <97156953+DeeKahy@users.noreply.github.com>
2026-02-10 18:13:16 +0100

    Add web-ext config; revert manifest to v2
    Add .web-ext-config.js to configure web-ext builds (ignore common folders/files and enable overwriteDest). Update manifest.json: change manifest_version from 3 to 2 and merge host_permissions into the permissions array (move the Path of Exile host entry), aligning the manifest with MV2 expectations.

---
+++ .web-ext-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  ignoreFiles: [
+    'docs/',
+    '.github/',
+    'web-ext-artifacts/',
+    '.git/',
+    '.gitignore',
+    'README.md',
+    '.web-ext-config.js'
+  ],
+  build: {
+    overwriteDest: true
+  }
+};
